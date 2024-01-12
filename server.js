ef4f25e350551dfd5f5563856949e848c260579f
Jess Skelton <jessskelton888@gmail.com>
2024-01-12 13:21:13 -0600

    started server.js code -- entered requireds, initialized mysql & database connections, and started async inquirer function

--- server.js
+++ server.js
@@ -1,2 +1,40 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+// MySQL Connection
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '', // Fill yours in here
+        database: 'company_db'
+    }
+);
+
+// Database Connection
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`Connected to the comapny_db database.`);
+    start(); 
+});
+
+// Function to start the app, called in database connection
+const start = async () => {
+    // Inquirer prompts
+    const answer = await inquirer.prompt([
+        {
+            // Main Menu -- choices
+            type: 'list',
+
+        }
+    ]);
+
+    switch (answer.action) {
+        // case -> choice
+            // await func();
+            // break;
+    }
+};
+
+// each choice's function
 

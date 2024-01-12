39e9554771bbe2ad0902f90740b3b4d5563653c2
Jess Skelton <jessskelton888@gmail.com>
2024-01-12 13:42:42 -0600

    completed start() with placeholders for bonus completion of assignment

--- server.js
+++ server.js
@@ -23,18 +23,78 @@ const start = async () => {
     // Inquirer prompts
     const answer = await inquirer.prompt([
         {
-            // Main Menu -- choices
+            // Main Menu
             type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View all Departments',
+                'View all Roles',
+                'View all Employees',
+                'Add a Department',
+                'Add a Role',
+                'Add an Employee',
+                'Update an Employee',
+                // 'View Employees by Manager',
+                // 'View Employees by Department',
+                // 'View total utilized budget',
+                // Update an Employee's Manager,
+                // Delete a Department, Role, or Employee,
+                'Exit'
+            ]
 
         }
     ]);
 
     switch (answer.action) {
-        // case -> choice
-            // await func();
+        case 'View all Departments':
+            await viewAllDepartments();
+            break;
+        case 'View all Roles':
+            await viewAllRoles();
+            break;
+        case 'View all Employees':
+            await viewAllEmployees();
+            break;
+        case 'Add a Department':
+            await addDepartment();
+            break;
+        case 'Add a Role':
+            await addRole();
+            break;
+        case 'Add an Employee':
+            await addEmployee();
+            break;
+        case 'Update an Employee':
+            await updateEmployee();
+            break;
+        // case 'View Employees by Manager':
+            // await viewByManager();
             // break;
+        // case 'View Employees by Department':
+            // await viewByDepartment();
+            // break;
+        // case 'View total utilized budget':
+            // await viewBudget();
+            // break;
+        // case Update an Employee's Manager:
+            // await updateManager();
+            // break;
+        // case Delete a Department, Role, or Employee: --- separate out???
+            // await deleteDRE();
+            // break;
+        case 'Exit':
+            connection.end();
+            break;
     }
 };
 
-// each choice's function
+// Each choice's functions --------------
+
+// Function to view all departments
+const viewAllDepartments = async () => {
+    
+};
+
+
 
